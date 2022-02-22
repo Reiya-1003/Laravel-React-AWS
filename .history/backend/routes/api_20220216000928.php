044f2e9fa528d40bb3b828533b0af1d0200872aa
Reiya-1003 <reiya.nico66@gmail.com>
2022-02-22 17:50:20 +0900

    laravel側でpost一覧の取得

--- .history/backend/routes/api_20220216000928.php
+++ .history/backend/routes/api_20220216000928.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'api'], function(){
-    Route::get('posts','App\Http\Controllers\Api\PostController@index');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
---
+++ backend/database/seeders/PostsTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        \DB::table('posts')->insert([
+
+            [
+                'name' => '森田ひかる',
+                'content' => 'MV撮影',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '東村芽依',
+                'content' => '雑誌撮影',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '清宮レイ',
+                'content' => '46時間生放送',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
+    }
+}
